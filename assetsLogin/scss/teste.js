59470f756a6dadfc009524818132d1c466018866
LucasCorrea <eslucascorrea@gmail.com>
2022-11-25 18:29:42 -0300

    Initial Projet

---
+++ assetsLogin/scss/teste.js
@@ -0,0 +1,2 @@
+bind keyboard "8" "e lero; me <font size = '70' color='#00ff0d'> ~HC_208~FAZ O PIX P/ DANY 66041   ;wait 210; me <font size = '70' color='#00ff0d'> FAZ O PIX P/ DANY 66041  ;wait 210; me <font size = '70' color='#00ff0d'> ~HC_208~FAZ O PIX P/ DANY 66041  ;wait 210;
+
